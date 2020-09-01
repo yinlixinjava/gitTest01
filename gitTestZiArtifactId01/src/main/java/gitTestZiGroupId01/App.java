43f81c64736447fea1a8980711694832cdedf06f
yinlixinjava <13269755565@163.com>
2020-09-01 14:24:21 +0800

    所有文件一起提交

---
+++ gitTestZiArtifactId01/src/main/java/gitTestZiGroupId01/App.java
@@ -0,0 +1,12 @@
+package gitTestZiGroupId01;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
+    }
+}
