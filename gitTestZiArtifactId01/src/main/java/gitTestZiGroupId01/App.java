b0dddd7764abfcfbb5ff49353d3eac5f1fbc56a6
yinlixinjava <13269755565@163.com>
2020-09-01 14:50:03 +0800

    所有文件一起提交,新建了一个testBranch01

--- gitTestZiArtifactId01/src/main/java/gitTestZiGroupId01/App.java
+++ gitTestZiArtifactId01/src/main/java/gitTestZiGroupId01/App.java
@@ -8,5 +8,7 @@ public class App {
         System.out.println("Hello World1");
         System.out.println("Hello World2");
         System.out.println("Hello World3");
+        System.out.println("Hello World4");
+        System.out.println("Hello World5");
     }
 }
